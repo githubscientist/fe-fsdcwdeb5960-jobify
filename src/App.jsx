d5f3c9299c028f09e930fb3d25a5f63ff10df366
githubscientist <sathish.datascientist@gmail.com>
2026-07-24 20:41:48 +0530

    auth flow --done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,49 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import { ToastContainer } from "react-toastify";
 import Home from "./pages/Home";
+import JobDetails from "./pages/JobDetails";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import UserDashboard from "./pages/UserDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+import RecruiterDashboard from "./pages/RecruiterDashboard";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />
+  },
+  {
+    path: "/job/:jobId",
+    element: <JobDetails />
+  },
+  {
+    path: "/register",
+    element: <Register />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/dashboard",
+    element: <UserDashboard />
+  },
+  {
+    path: "/admin/dashboard",
+    element: <AdminDashboard />
+  },
+  {
+    path: "/recruiter/dashboard",
+    element: <RecruiterDashboard />
   }
 ])
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <RouterProvider router={router} />
       <ToastContainer 
         position="top-right"
@@ -25,7 +57,7 @@ const App = () => {
         pauseOnHover
         theme="light"
       />
-    </>
+    </Provider>
   )
 }
 
